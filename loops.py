3813e9c7d199f7ee4e4472499eefb381b0e7fb87
mariniou77 <antonis.marinidis7@gmail.com>
2022-03-18 12:21:13 +0100

    add for loop

--- loops.py
+++ loops.py
@@ -4,4 +4,27 @@ while i <= 10:
     print(i)
     i += 1
 
+# for loop
+# in this example you loop as many letters as the string given
+for letter in "Greece Thessaloniki":
+    # print every value that variable letter takes each time
+    print(letter)
 
+# for loop in arrays    
+friends = ["Antonis", "Xristos", "Dimitris"]
+for friend in friends:
+    # print every element of the array
+    print(friend)
+
+# another way to print every element of an array depending on the length of the array
+for index in range(len(friends)):
+    # print every element of the array
+    print(friends[index])    
+
+# for loop in a range of numbers
+for i in range(10):
+    print(i)    
+
+# for loop in a specific range of numbers
+for i in range(3, 10):
+    print(i)      
