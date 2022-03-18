47eaab2185a7719261e311ce7b5075266fdc9450
mariniou77 <antonis.marinidis7@gmail.com>
2022-03-18 12:08:22 +0100

    add some comments

--- guess_game.py
+++ guess_game.py
@@ -4,6 +4,7 @@ times_guess = 0
 guess_limit = 3
 out_of_guesses = False
 
+# loop until you guess the correct word or until you are out of tries
 while guess_word != secret_word and not(out_of_guesses):
     if times_guess < guess_limit:
         guess_word = input("Enter your guess : ")
