dc4ac0a40195a7e49464e3f1606c2d55c3a322c2
mariniou77 <antonis.marinidis7@gmail.com>
2022-05-06 11:02:38 +0200

    add a class function

--- basics_in_python/classes/main.py
+++ basics_in_python/classes/main.py
@@ -1,8 +1,16 @@
 from Student import Student
 
-student1 = Student("Antonis", "Data Science", 2.5, True)
-student2 = Student("Dimitris", "Engineering", 3.1, False)
+student1 = Student("Antonis", "Data Science", 2.5)
+student2 = Student("Dimitris", "Engineering", 3.1)
 
 print("Student n.1 : \nName : " + student1.name + "\nGPA : " + str(student1.gpa))
+if student1.pass_grade():
+    print("Pass : Yes")
+else :
+    print("Pass : No")    
 print("------------------------------")
 print("Student n.2 : \nName : " + student2.name + "\nGPA : " + str(student2.gpa))
+if student2.pass_grade():
+    print("Pass : Yes")
+else :
+    print("Pass : No") 
