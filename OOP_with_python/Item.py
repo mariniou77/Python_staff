b0a67dd40fe3c5ef8cc2325dfa603e98578cc069
mariniou77 <antonis.marinidis7@gmail.com>
2022-05-13 12:16:37 +0200

    add some comments

--- OOP_with_python/Item.py
+++ OOP_with_python/Item.py
@@ -1,3 +1,5 @@
+import csv
+
 class Item:
     pay_rate = 0.8
     all = []
@@ -17,7 +19,22 @@ class Item:
         return self.price * self.quantity   
 
     def discount(self):
-        self.price = self.price * self.pay_rate    
+        self.price = self.price * self.pay_rate   
+
+    # With this class method I am reading the products from a csv file and immidiatle I am making instances of them  
+    @classmethod
+    def instantiate_from_csv(cls):
+        # reading the products
+        with open('OOP_with_python/products.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            products = list(reader)
+        # Making the instances
+        for item in products:
+            Item(
+                name = item.get("name"),
+                price = float(item.get("price")),
+                quantity = int(item.get("quantity"))
+            )
 
     # With this magic function we have a prettier and more understanding view of all the instances we put in the list "all"
     def __repr__(self):
--- OOP_with_python/shop.py
+++ OOP_with_python/shop.py
@@ -1,16 +1,20 @@
 from Item import Item
 
-item1 = Item("Phone", 350, 3)
-item2 = Item("Laptop", 1000, 6)
-item3 = Item("Monitor", 220, 2)
-item4 = Item("HDMI", 23, 10)
-item5 = Item("Mouse", 18, 8)
+# item1 = Item("Phone", 350, 3)
+# item2 = Item("Laptop", 1000, 6)
+# item3 = Item("Monitor", 220, 2)
+# item4 = Item("HDMI", 23, 10)
+# item5 = Item("Mouse", 18, 8)
 
-# Print all the items we have in our shop
+# # Print all the items we have in our shop
+# print(Item.all)
+# # Print the price of item1 before discount
+# print(item1.price)
+# # Apply the discount
+# item1.discount()
+# # Print the price of item1 after discount
+# print(item1.price)
+
+# If I want to make the instances from the csv file I have to comment out the lines that making manually instances 
+Item.instantiate_from_csv()
 print(Item.all)
-# Print the price of item1 before discount
-print(item1.price)
-# Apply the discount
-item1.discount()
-# Print the price of item1 after discount
-print(item1.price)
